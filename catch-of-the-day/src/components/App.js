31c7c7672f2cbe78e32508d4dec1cfdd2868f886
Cesar Gonzalez <cgonzalez.argandar@gmail.com>
2020-01-30 15:35:26 -0800

    16 - Updating our Order State

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
 
     addFish = fish => {
 
+        debugger;
         //  1. Take a copy of the existing state
         const fishes = { ...this.state.fishes };
 
@@ -31,17 +32,30 @@ class App extends React.Component {
         })
     };
 
+    addToOrder = key => {
+
+        // 1. take a copy of state
+        const order = { ...this.state.order };
+
+        // 2. Either add to the order, or update the number in our order
+        order[key] = order[key] + 1 || 1;
+
+        // 3. Call setSate to update our state object
+
+        this.setState({ order });
+    };
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
                     <ul className="fishes">
-                        { Object.keys(this.state.fishes).map(key => <Fish key={ key } details={ this.state.fishes[key] } />) }
+                        { Object.keys(this.state.fishes).map(key => <Fish index={key} key={ key } details={ this.state.fishes[key] } addToOrder={ this.addToOrder } />) }
                     </ul>
                 </div>
                 <Order />
-                <Inventory addFish={ this.addFish } loadSamplesFishes={ this.loadSamplesFishes }/>
+                <Inventory addFish={ this.addFish } loadSamplesFishes={ this.loadSamplesFishes } />
             </div>
         );
     }
--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -7,6 +7,7 @@ class Fish extends React.Component {
     render() {
 
         const { image, name, price, desc, status } = this.props.details;
+        const isAvailable = status === 'available';
 
 
         return (
@@ -17,7 +18,7 @@ class Fish extends React.Component {
                     <span className="price">{ formatPrice(price) }</span>
                 </h3>
                 <p>{ desc }</p>
-                <button>Add To Cart</button>
+                <button disabled={ !isAvailable } onClick={ () => {this.props.addToOrder(this.props.index)} }>{ isAvailable ? 'Add To Cart' : 'Sold out'}</button>
             </li>
         );
     }
