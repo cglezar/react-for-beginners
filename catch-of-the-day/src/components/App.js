825b2e631bd7172fe43d522b0d804b80efd64077
Cesar Gonzalez <cgonzalez.argandar@gmail.com>
2020-01-30 14:45:32 -0800

    15 - Displaying State with JSX

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,18 +2,46 @@ import React from 'react';
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import sampleFishes from '../sample-fishes';
+import Fish from "./Fish";
 
 class App extends React.Component {
 
-    render() {
+    state = {
+        fishes: {},
+        order: {}
+    };
 
+    addFish = fish => {
+
+        //  1. Take a copy of the existing state
+        const fishes = { ...this.state.fishes };
+
+        // 2. Add new fishes to that fishes variable
+        fishes[`fish${ Date.now() }`] = fish;
+
+        // 3 Set the new fishes object to state
+        this.setState({ fishes });
+    };
+
+    loadSamplesFishes = () => {
+
+        this.setState({
+            fishes: sampleFishes
+        })
+    };
+
+    render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        { Object.keys(this.state.fishes).map(key => <Fish key={ key } details={ this.state.fishes[key] } />) }
+                    </ul>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={ this.addFish } loadSamplesFishes={ this.loadSamplesFishes }/>
             </div>
         );
     }
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -3,12 +3,31 @@ import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
 
+    myInput = React.createRef();
+
+    goToStore = (event) => {
+
+        // 1. Stop form from submitting
+        event.preventDefault();
+
+        // 2. Get text from input
+        const storeName = this.myInput.current.value;
+
+        // 3. Change the cpage to /store/whatever-they-entered  
+        this.props.history.push(`/store/${storeName}`);
+    };
+
     render() {
 
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store name" defaultValue={ getFunName() } />
+                <input
+                    type="text"
+                    ref={this.myInput}
+                    required
+                    placeholder="Store name"
+                    defaultValue={getFunName()} />
                 <button type="submit">Visit Store -></button>
             </form>
         );
